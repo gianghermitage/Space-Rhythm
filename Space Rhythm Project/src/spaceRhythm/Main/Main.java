8adca47d45f29e9f3afc15bc94316d6155dfa7bf
giang <trgiang1998@gmail.com>
2019-03-24 20:12:47 +0700

    commit ver 0.2

---
+++ Space Rhythm Project/src/spaceRhythm/Main/Main.java
@@ -0,0 +1,10 @@
+package spaceRhythm.Main;
+
+import spaceRhythm.Game.Game;
+
+
+public class Main {
+    public static void main(String[] args) {
+        new Game();
+    }
+}
---
+++ Space Rhythm Project/src/spaceRhythm/game/GameObject.java
@@ -0,0 +1,63 @@
+package spaceRhythm.Game;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected ObjectID ID;
+    protected int velX = 0;
+    protected int velY = 0;
+
+
+    public abstract void tick();
+    public abstract void render(Graphics g);
+    public abstract Rectangle getBounds();
+
+    public GameObject(int x, int y, ObjectID ID){
+        this.ID = ID;
+        this.x = x;
+        this.y = y;
+    }
+
+    public ObjectID getID() {
+        return ID;
+    }
+
+    public void setID(ObjectID ID) {
+        this.ID = ID;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVelX() {
+        return velX;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public int getVelY() {
+        return velY;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+
+}
