30d8fe1d8871511391546524c2ce3852585ea0fb
giang <trgiang1998@gmail.com>
2019-03-24 20:18:04 +0700

    commit ver 0.2

---
+++ Space Rhythm Project/src/spaceRhythm/Game/Handler.java
@@ -0,0 +1,65 @@
+package spaceRhythm.Game;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+public class Handler {
+    public LinkedList<GameObject> object = new LinkedList<GameObject>();
+    private boolean up = false, down =false, left = false, right = false;
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void tick(){
+        for (int i = 0, objectSize = object.size(); i < objectSize; i++) {
+            GameObject tempObject = object.get(i);
+            tempObject.tick();
+        }
+    }
+
+    public void render(Graphics g){
+        for (int i = 0, objectSize = object.size(); i < objectSize; i++) {
+            GameObject tempObject = object.get(i);
+            tempObject.render(g);
+        }
+    }
+
+    public void addObject(GameObject tempObject){
+        object.add(tempObject);
+    }
+
+    public void removeObject(GameObject tempObject){
+        object.remove(tempObject);
+    }
+
+
+}
---
+++ Space Rhythm Project/src/spaceRhythm/Game/ObjectID.java
@@ -0,0 +1,9 @@
+package spaceRhythm.Game;
+
+public enum ObjectID {
+    Player(),
+    Block(),
+    PickUp(),
+    Bullet(),
+    Enemy()
+}
