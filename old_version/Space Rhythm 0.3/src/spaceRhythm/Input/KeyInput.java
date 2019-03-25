c3a934bd86de677f755d4dd60ce5d4aa59626ffd
giang <trgiang1998@gmail.com>
2019-03-25 16:43:19 +0700

    commit old_version 0.3

---
+++ old_version/Space Rhythm 0.3/src/spaceRhythm/Input/KeyInput.java
@@ -0,0 +1,42 @@
+package spaceRhythm.Input;
+
+import spaceRhythm.Game.GameObject;
+import spaceRhythm.Game.Handler;
+import spaceRhythm.Game.ObjectID;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+    private Handler handler;
+
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            if (tempObject.getID() == ObjectID.Player) {
+                if (key == KeyEvent.VK_W) handler.setUp(true);
+                if (key == KeyEvent.VK_S) handler.setDown(true);
+                if (key == KeyEvent.VK_A) handler.setLeft(true);
+                if (key == KeyEvent.VK_D) handler.setRight(true);
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            if (tempObject.getID() == ObjectID.Player) {
+                if (key == KeyEvent.VK_W) handler.setUp(false);
+                if (key == KeyEvent.VK_S) handler.setDown(false);
+                if (key == KeyEvent.VK_A) handler.setLeft(false);
+                if (key == KeyEvent.VK_D) handler.setRight(false);
+            }
+        }
+    }
+}
