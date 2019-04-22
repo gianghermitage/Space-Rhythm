7017e18d86ff65fecef1fb7dd4f2b2a99b4cf6df
Nguyen Tuan Hai <nth23798@gmail.com>
2019-04-22 23:07:54 +0700

    commit ver 0.7

--- Space Rhythm Project/src/spaceRhythm/Main/Main.java
+++ Space Rhythm Project/src/spaceRhythm/Main/Main.java
@@ -1,10 +1,30 @@
 package spaceRhythm.Main;
 
-import spaceRhythm.Game.Game;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
 
-public class Main {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("menu.fxml"));
+
+            primaryStage.setTitle("Space Rhythm Launcher");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        new Game();
+        launch(args);
     }
 }
